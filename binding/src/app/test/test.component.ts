20cc270be31cabeab4380f3271f7d7bb1d5ce68c
Monayem Islam <monayemislam@gmail.com>
2022-08-26 23:37:43 +0600

    Event Binding

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -3,9 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `<h2>Welcome {{name}}</h2>
-             <h2 [style.color]="bindingColor">Style Binding</h2>
-             <h2 [style.color]="hasError?'red':'green'">Style Binding</h2>
-             <h2 [ngStyle]="messageStyle">Style Binding</h2>
+             <button (click)="onClick()">Greet</button>
+             {{greetings}}
             `,
   styles: [`
 
@@ -14,13 +13,11 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
 
   public name = "Monayem";
-  public hasError:boolean = false;
-  public bindingColor = "red";
-  public messageStyle = {
-    "color": "yellow",
-    "backgroundColor" : "green"
+  public greetings = "";
+  public onClick() {
+    this.greetings = "Greetings from"+this.name;
+    console.log("hello");
   }
-  
   constructor() { }
 
   ngOnInit(): void {
