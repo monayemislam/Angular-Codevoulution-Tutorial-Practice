2610fb408682431111cfb25e026cfb2db200cbd1
Monayem Islam <monayemislam@gmail.com>
2022-08-26 23:22:47 +0600

    Style Binding

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -3,37 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `<h2>Welcome {{name}}</h2>
-             <h2 class="text-success">Codevolution</h2>
-             <h2 [class]="successClass">Codevolution</h2>
-             <h2 [class.text-danger]="hasError">Codevolution</h2>
-             <h2 [ngClass]="messageClasses">Codevolution</h2>
+             <h2 [style.color]="bindingColor">Style Binding</h2>
+             <h2 [style.color]="hasError?'red':'green'">Style Binding</h2>
+             <h2 [ngStyle]="messageStyle">Style Binding</h2>
             `,
   styles: [`
-      .text-success{
-        color: darkgreen;
-      }
-      .text-danger{
-        color: darkred;
-      }
-      .text-special{
-        border: 2px solid red;
-      }
+
   `]
 })
 export class TestComponent implements OnInit {
 
   public name = "Monayem";
-  public successClass = "text-success";
-  public hasError:boolean = true;
-  public messageClasses = {
-    "text-success" : !this.hasError,
-    "text-special" : this.successClass
+  public hasError:boolean = false;
+  public bindingColor = "red";
+  public messageStyle = {
+    "color": "yellow",
+    "backgroundColor" : "green"
   }
+  
   constructor() { }
 
   ngOnInit(): void {
-  }
-  greetUser(){
-    return "Hello "+this.name
   }
 }
