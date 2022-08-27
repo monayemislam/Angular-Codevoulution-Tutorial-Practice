54de9173a2602e472b0ff8baa6feee11a9d17d7c
Monayem Islam <monayemislam@gmail.com>
2022-08-27 10:06:27 +0600

    If Else-Structural Behavior

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -2,22 +2,31 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `<h2>Welcome {{name}}</h2>
-              <input #myId type="text">
-             <button (click)="logMessage(myId.value)">Log</button>
+  template: `
+              <h2 *ngIf="true">Monayem Islam</h2>
+              <h2 *ngIf="false">Monayem Islam (Hidden)</h2>
+
+              <h2 *ngIf="displayName; else elseBlock">Codevolution</h2>
+              <ng-template #elseBlock>
+                 <h2>Codevolution from else block</h2>
+              </ng-template>
+
+            <div *ngIf="displayName;then thenBlock; else elseBlock"></div>
+            <ng-template #thenBlock>
+                 <h2>Codevolution from else block</h2>
+              </ng-template>
+            <ng-template #elseBlock>
+                 <h2>Codevolution from else block</h2>
+            </ng-template>
+
             `,
   styles: [`
 
   `]
 })
 export class TestComponent implements OnInit {
-
-  public name = "Monayem";
-  public logMessage(logMessageData: any) {
-    console.log(logMessageData);
-  }
   constructor() { }
-
+  public displayName = false;
   ngOnInit(): void {
   }
 }
