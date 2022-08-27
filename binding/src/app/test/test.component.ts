60f6816769444de6dd80a125386db64538ae6356
Monayem Islam <monayemislam@gmail.com>
2022-08-27 11:44:04 +0600

    Switch-Structural behavior

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -3,21 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-              <h2 *ngIf="true">Monayem Islam</h2>
-              <h2 *ngIf="false">Monayem Islam (Hidden)</h2>
-
-              <h2 *ngIf="displayName; else elseBlock">Codevolution</h2>
-              <ng-template #elseBlock>
-                 <h2>Codevolution from else block</h2>
-              </ng-template>
-
-            <div *ngIf="displayName;then thenBlock; else elseBlock"></div>
-            <ng-template #thenBlock>
-                 <h2>Codevolution from else block</h2>
-              </ng-template>
-            <ng-template #elseBlock>
-                 <h2>Codevolution from else block</h2>
-            </ng-template>
+              <div [ngSwitch]="color">
+                  <div *ngSwitchCase="'blue'">You have choosen blue color</div>
+                  <div *ngSwitchCase="'red'">You have choosen red color</div>
+                  <div *ngSwitchDefault>You have choosen default color</div>
+              </div>
 
             `,
   styles: [`
@@ -26,7 +16,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
   constructor() { }
-  public displayName = false;
+  public color = "abllgjklj";
   ngOnInit(): void {
   }
 }
